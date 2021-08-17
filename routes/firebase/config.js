37c5d2b882b6b1ce18b9b261ac5b4d4cf7926cdd
JS-hong <lielty9371@gmail.com>
2021-08-18 01:46:26 +0900

    a

--- routes/firebase/config.js
+++
@@ -1,7 +0,0 @@
-<body>
-
-<script src="/__/firebase/8.9.1/firebase-app.js"></script>
-<script src="/__/firebase/8.9.1/firebase-analytics.js"></script>
-<script src="/__/firebase/init.js"></script>
-
-</body>
