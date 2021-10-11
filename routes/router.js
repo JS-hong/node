163f69316bfda0ee99fcd9c4dc7bb11f65a4b4a4
JS-hong <lielty9371@gmail.com>
2021-10-11 19:24:09 +0900

    router

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
-const database = require('./firebase/config');
-const reqLogin = require('./requestLogin');
 
 router.get('/', (req, res) => {
     res.render('index', {title: "인덱스"})
