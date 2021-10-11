c0a8bdb9e71419210b46850dc52a9c36d39874ec
JS-hong <lielty9371@gmail.com>
2021-10-11 19:16:27 +0900

    a

--- routes/requestSingUp.js
+++ routes/requestSingUp.js
@@ -1,32 +1,25 @@
 const express = require('express')
 const router = express.Router()
-const firebaseConfig = {
-    apiKey: "AIzaSyBKSB7qLEtAJyYrPUwLjMKsfRnFnF4faD4",
-    authDomain: "helloworld-4d72e.firebaseapp.com",
-    databaseURL: "https://helloworld-4d72e-default-rtdb.firebaseio.com",
-    projectId: "helloworld-4d72e",
-    storageBucket: "helloworld-4d72e.appspot.com",
-    messagingSenderId: "744612360228",
-    appId: "1:744612360228:web:ad72f1b0228bb9acd228b1",
-    measurementId: "G-J36G382S67"  
-  };
-firebase.initializeApp(firebaseConfig);
-    
+const db = require('./mysqlconnect');   
 
 router.post('/requestSignUp', function(req,res) {
 
-    const email = req.body.email; //id post 수신
-    const password = req.body.password; // password 수신
+    const uid = req.body.uid;
 
-            firebase.auth().createUserWithEmailAndPassword(email, password)
-                .then(() => {
-                    alert('회원가입이 완료되었습니다.')
-                })
-                .catch(function (error) {
-                    var errorCode = error.code;
-                    var errorMessage = error.message;
-                    alert(error.code)
-                });
-        })
+    var sql = 'INSERT INTO userDB(user_id) VALUES(?)';
+	var params = [uid];
+	
+	db.query(sql,params,function(err, rows){
+		if (err) throw err;
+		if (rows)
+        {
+            res.send({data : success});
+		} 
+        else 
+        {
+            res.send({data : fail});
+		}
+	})
+})
 
 module.exports = requestSignUp;
