85df739a1783f2d1dc463eac88d52b7844b82e8e
JS-hong <lielty9371@gmail.com>
2021-10-11 18:58:45 +0900

    a

--- routes/requestLogin.js
+++ routes/requestLogin.js
@@ -1,58 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const firebase = require("firebase");
-const app = express();
-
-app.use(express.json());
-
-const firebaseConfig = {
-    apiKey: "AIzaSyBKSB7qLEtAJyYrPUwLjMKsfRnFnF4faD4",
-    authDomain: "helloworld-4d72e.firebaseapp.com",
-    databaseURL: "https://helloworld-4d72e-default-rtdb.firebaseio.com",
-    projectId: "helloworld-4d72e",
-    storageBucket: "helloworld-4d72e.appspot.com",
-    messagingSenderId: "744612360228",
-    appId: "1:744612360228:web:ad72f1b0228bb9acd228b1",
-    measurementId: "G-J36G382S67"  
-  };
+const db = require('./mysqlconnect');
 
 router.post('/requsetLogin', function(req, res) { 
-   	
-    const email = req.body.email;//id post 수신
-    const password = req.body.password; // password 수
-   
-    console.log(req.body);
-    console.log(email);
-    console.log(password);
-    
-    if (!firebase.apps.length) {
-        firebase.initializeApp(firebaseConfig)
-    } else {
-        firebase.app() // 이미 초기화되었다면, 초기화 된 것을 사용함
-    }
 
-    firebase.auth().signInWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-        var user = userCredential.user;
-    })
-    .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        alert(errorMessage);
-    });
-/*
-    if(req.session.user){
-        console.log('세션이 이미 존재합니다.');
-    }    
-    else{   
-        req.session.user = {
-            "userId" : userId,
-        }        
-    var userId = req.query.user;
-        console.log('세션 저장 완료!');
-    }
-*/
-        res.send({  data: 'success'  });
+    const uid = req.body.uid;
+
+    var sql = 'INSERT INTO userDB(user_id) VALUES(?)';
+	var params = [uid];
+	
+	db.query(sql,params,function(err, rows){
+		if (err) throw err;
+		if (rows)
+        {
+            res.send({data : success});
+		} 
+        else 
+        {
+            res.send({data : fail});
+		}
+	})
 });
 
 module.exports = router;
