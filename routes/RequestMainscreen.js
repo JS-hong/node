7379d588ab07f4d57f76e4d8962aad61fb12697c
JS-hong <lielty9371@gmail.com>
2021-10-13 10:41:26 +0900

    a

--- routes/RequestMainscreen.js
+++ routes/RequestMainscreen.js
@@ -16,34 +16,34 @@ router.get('/requestmainscreen', function(req,res) {
             res.send(
                 [
                     {
-                        id_1 : rows[0].id,
+                        id_1 : rows[0].user_id,
                         write_time_1 : rows[0].write_time,
                         subtext_1 : rows[0].subtext,
                         tag_1 : rows[0].tag,
                         post_id_1 : rows[0].post_id
                     },
                     {
-                        id_2 : rows[1].id,
+                        id_2 : rows[1].user_id,
                         write_time_2 : rows[1].write_time,
                         subtext_2 : rows[1].subtext,
                         tag_2 : rows[1].tag,
                         post_id_2 : rows[1].post_id
                     },
                     {
-                        id_3 : rows[2].id,
+                        id_3 : rows[2].user_id,
                         write_time_3 : rows[2].write_time,
                         subtext_3 : rows[2].subtext,
                         tag_3 : rows[2].tag,
                         post_id_3 : rows[2].post_id
                     },
                     {
-                        id_4 : rows[3].id,
+                        id_4 : rows[3].user_id,
                         write_time_4 : rows[3].write_time,
                         subtext_4 : rows[3].subtext,
                         tag_4 : rows[3].tag,
                         post_id_4 : rows[3].post_id
                     },                    {
-                        id_5 : rows[4].id,
+                        id_5 : rows[4].user_id,
                         write_time_5 : rows[4].write_time,
                         subtext_5 : rows[4].subtext,
                         tag_5 : rows[4].tag,
