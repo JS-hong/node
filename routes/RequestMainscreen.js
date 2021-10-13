2945b3d505efc6a9adb3d742040d0a09a513a4bf
JS-hong <lielty9371@gmail.com>
2021-10-13 11:30:28 +0900

    a

--- routes/RequestMainscreen.js
+++ routes/RequestMainscreen.js
@@ -13,40 +13,46 @@ router.get('/requestmainscreen', function(req,res) {
 		if (err) throw err;
 		if (rows){
             res.send(
+                "feeds"  
                 [
                     {
-                        id_1 : rows[0].user_id,
-                        write_time_1 : rows[0].write_time,
-                        subtext_1 : rows[0].subtext,
-                        tag_1 : rows[0].tag,
-                        post_id_1 : rows[0].post_id
+                        uid : rows[0].user_id,
+                        write_time : rows[0].write_time,
+                        title : rows[0].subtext,
+                        post_tag : rows[0].tag,
+                        content_language : rows[0].language_type,
+                        post_id : rows[0].post_id
                     },
                     {
-                        id_2 : rows[1].user_id,
-                        write_time_2 : rows[1].write_time,
-                        subtext_2 : rows[1].subtext,
-                        tag_2 : rows[1].tag,
-                        post_id_2 : rows[1].post_id
+                        uid : rows[1].user_id,
+                        write_time : rows[1].write_time,
+                        title : rows[1].subtext,
+                        post_tag : rows[1].tag,
+                        content_language : rows[1].language_type,
+                        post_id : rows[1].post_id
                     },
                     {
-                        id_3 : rows[2].user_id,
-                        write_time_3 : rows[2].write_time,
-                        subtext_3 : rows[2].subtext,
-                        tag_3 : rows[2].tag,
-                        post_id_3 : rows[2].post_id
+                        uid : rows[2].user_id,
+                        write_time : rows[2].write_time,
+                        title : rows[2].subtext,
+                        post_tag : rows[2].tag,
+                        content_language : rows[2].language_type,
+                        post_id : rows[2].post_id
                     },
                     {
-                        id_4 : rows[3].user_id,
-                        write_time_4 : rows[3].write_time,
-                        subtext_4 : rows[3].subtext,
-                        tag_4 : rows[3].tag,
-                        post_id_4 : rows[3].post_id
+                        uid : rows[3].user_id,
+                        write_time : rows[3].write_time,
+                        title : rows[3].subtext,
+                        post_tag : rows[3].tag,
+                        content_language : rows[3].language_type,
+                        post_id : rows[3].post_id
                     },                    {
-                        id_5 : rows[4].user_id,
-                        write_time_5 : rows[4].write_time,
-                        subtext_5 : rows[4].subtext,
-                        tag_5 : rows[4].tag,
-                        post_id_5 : rows[4].post_id
+                        uid : rows[4].user_id,
+                        write_time : rows[4].write_time,
+                        title : rows[4].subtext,
+                        post_tag : rows[4].tag,
+                        content_language : rows[4].language_type,
+                        post_id : rows[4].post_id
                     }                    
                 ]);
 		} 
