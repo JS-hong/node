1129831668534f8980981d3df18c00c1b6d6725a
JS-hong <lielty9371@gmail.com>
2021-10-19 19:45:07 +0900

    a

--- routes/RequestMainscreen.js
+++ routes/RequestMainscreen.js
@@ -23,7 +23,6 @@ router.get('/requestmainscreen', function(req,res) {
                 [
                     {
                         uid : rows[0].user_id,
-                        write_time : rows[0].write_time,
                         title : rows[0].subtext,
                         post_tag : rows[0].tag,
                         language_type : rows[0].language_type,
@@ -33,11 +32,11 @@ router.get('/requestmainscreen', function(req,res) {
                         writer_nickname : rows[0].writer_nickname,
                         writer_thumbnail : rows[0].writer_thumbnail,
                         line_of_code : rows[0].line_of_code,
-                        bookmark_saved : rows[0].bookmark_saved
+                        bookmark_saved : rows[0].bookmark_saved,
+                        bookmark : rows[0].bookmark
                     },
                     {
                         uid : rows[1].user_id,
-                        write_time : rows[1].write_time,
                         title : rows[1].subtext,
                         post_tag : rows[1].tag,
                         language_type : rows[1].language_type,
@@ -47,11 +46,11 @@ router.get('/requestmainscreen', function(req,res) {
                         writer_nickname : rows[1].writer_nickname,
                         writer_thumbnail : rows[1].writer_thumbnail,
                         line_of_code : rows[1].line_of_code,
-                        bookmark_saved : rows[1].bookmark_saved
+                        bookmark_saved : rows[1].bookmark_saved,
+                        bookmark : rows[1].bookmark
                     },
                     {
                         uid : rows[2].user_id,
-                        write_time : rows[2].write_time,
                         title : rows[2].subtext,
                         post_tag : rows[2].tag,
                         language_type : rows[2].language_type,
@@ -61,11 +60,11 @@ router.get('/requestmainscreen', function(req,res) {
                         writer_nickname : rows[2].writer_nickname,
                         writer_thumbnail : rows[2].writer_thumbnail,
                         line_of_code : rows[2].line_of_code,
-                        bookmark_saved : rows[2].bookmark_saved
+                        bookmark_saved : rows[2].bookmark_saved,
+                        bookmark : rows[2].bookmark
                     },
                     {
                         uid : rows[3].user_id,
-                        write_time : rows[3].write_time,
                         title : rows[3].subtext,
                         post_tag : rows[3].tag,
                         language_type : rows[3].language_type,
@@ -75,11 +74,11 @@ router.get('/requestmainscreen', function(req,res) {
                         writer_nickname : rows[3].writer_nickname,
                         writer_thumbnail : rows[3].writer_thumbnail,
                         line_of_code : rows[3].line_of_code,
-                        bookmark_saved : rows[3].bookmark_saved
+                        bookmark_saved : rows[3].bookmark_saved,
+                        bookmark : rows[3].bookmark
                     },                    
                     {
                         uid : rows[4].user_id,
-                        write_time : rows[4].write_time,
                         title : rows[4].subtext,
                         post_tag : rows[4].tag,
                         language_type : rows[4].language_type,
@@ -89,7 +88,8 @@ router.get('/requestmainscreen', function(req,res) {
                         writer_nickname : rows[4].writer_nickname,
                         writer_thumbnail : rows[4].writer_thumbnail,
                         line_of_code : rows[4].line_of_code,
-                        bookmark_saved : rows[4].bookmark_saved
+                        bookmark_saved : rows[4].bookmark_saved,
+                        bookmark : rows[4].bookmark
                     }                    
                 ]});
 		} 
