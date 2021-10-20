1826708a4e5de09892367597f3deced062bbedef
JS-hong <lielty9371@gmail.com>
2021-10-20 10:04:20 +0900

    a

--- routes/GetDetailPostResult.js
+++ routes/GetDetailPostResult.js
@@ -5,7 +5,7 @@ router.post('/getdetailpostresult', function(req,res) {
 
     const postid = req.body.post_id; //post data 받음
 
-    var sql = 'select * postDB where post_id=?';
+    var sql = 'select * PostwriteDB where post_id=?';
 	var params = [postid];
 	
 	db.query(sql,params,function(err,rows){
@@ -13,13 +13,7 @@ router.post('/getdetailpostresult', function(req,res) {
 		if (rows){
             res.send({
                         post_id : rows[0].post_id,
-                        id : rows[0].id,
                         maintext : rows[0].maintext,
-                        subtext : rows[0].subtext,
-                        lineofcode : rows[0].lineofcode,
-                        write_time : rows[0].write_time,
-                        tag : rows[0].tag,
-                        language_type : rows[0].language_type
                     });
 		} 
         else {
