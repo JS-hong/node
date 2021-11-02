7f564ca1ef524ab66f97a54e89f423cc395fa0bf
JS-hong <lielty9371@gmail.com>
2021-11-02 20:32:00 +0900

    a

--- routes/cancleBookmark.js
+++ routes/cancleBookmark.js
@@ -7,8 +7,8 @@ router.post('/canclebookmark', function(req,res) {
     const uid = req.body.user_id;
     const postid = req.body.post_id;
 
-    var sql = 'delete bookmark_db where post_id=?';
-	var params = [postid];
+    var sql = 'delete bookmark_db where post_id=? AND user_id=? ';
+	var params = [postid,uid];
 
 
     connection.query(sql,params,function(err, rows){
