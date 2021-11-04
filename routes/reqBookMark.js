cb0d20c6a68b94e90936a31930812fe3e37ad1a3
JS-hong <lielty9371@gmail.com>
2021-11-04 20:27:45 +0900

    a

--- routes/reqBookMark.js
+++ routes/reqBookMark.js
@@ -8,11 +8,11 @@ router.post('/reqBookmark', function(req,res) {
 
     var sql = "select PostwriteDB.post_id,bookmark_db.bookmark, "
     + "PostwriteDB.user_id,PostwriteDB.subtext,PostwriteDB.tag, "
-    + "PostwriteDB.language_type,PostwriteDB.write_time,PostwriteDB.writer_nickname "
+    + "PostwriteDB.language_type,PostwriteDB.write_time,PostwriteDB.writer_nickname, "
     + "PostwriteDB.writer_thumbnail,PostwriteDB.language_thumbnail,PostwriteDB.line_of_code,PostwriteDB.bookmark_saved "
     + "from PostwriteDB left join bookmark_db "
     + "on PostwriteDB.post_id = bookmark_db.post_id "
-    + "where PostwriteDB.user_id=? ";
+    + "where bookmark_db.user_id=? ";
 
 	var params = [uid];
 
