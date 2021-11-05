d0d2778f33f4e11ad6890aabdd2f729eab11a808
JS-hong <lielty9371@gmail.com>
2021-11-05 19:50:15 +0900

    a

--- routes/RequestMainscreen.js
+++ routes/RequestMainscreen.js
@@ -43,10 +43,25 @@ router.post('/requestmainscreen', function(req,res) {
                         })
                         
                         randomIndexArray.push(rows[random].post_id)    
-                    n++;  
+                        n++;  
                 }
                 else{
-                    n--;
+                    json.pop({
+                        uid : rows[random].user_id,
+                        title : rows[random].subtext,
+                        post_tag : rows[random].tag,
+                        language_type : rows[random].language_type,
+                        language_thumbnails : rows[random].language_thumbnail,
+                        post_id : rows[random].post_id,
+                        write_time : rows[random].write_time,
+                        writer_nickname : rows[random].writer_nickname,
+                        writer_thumbnail : rows[random].writer_thumbnail,
+                        line_of_code : rows[random].line_of_code,
+                        bookmark_saved : rows[random].bookmark_saved,
+                        bookmark : rows[random].bookmark
+                        })
+                    randomIndexArray.pop(rows[random].post_id)
+                    n--; 
                 }
             }
             res.send({"feeds": json})
