4959b868157ac73541211b2f7c016092bb258af3
JS-hong <lielty9371@gmail.com>
2021-11-05 17:54:11 +0900

    a

--- routes/RequestMainscreen.js
+++ routes/RequestMainscreen.js
@@ -20,7 +20,7 @@ router.post('/requestmainscreen', function(req,res) {
 	connection.query(sql,params,function(err,rows,fields){
 		if (err) throw err;
         if (rows){
-            var random = Math.random * rows.length;
+            var random = Math.random() * rows.length;
             console.log(random)
             var n = 0;
             while(n<5){
