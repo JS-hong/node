f4aaefb05fcf0d798d144205d49e7dec7d4f4693
JS-hong <lielty9371@gmail.com>
2021-11-05 19:45:55 +0900

    a

--- routes/RequestMainscreen.js
+++ routes/RequestMainscreen.js
@@ -26,7 +26,7 @@ router.post('/requestmainscreen', function(req,res) {
             while(n<5){
                 var random = Math.floor(Math.random()*rows.length);
                 if(randomIndexArray.indexOf(random) === -1){
-                    console.log(arr)
+                    console.log(randomIndexArray)
                     json.push({
                         uid : rows[random].user_id,
                         title : rows[random].subtext,
