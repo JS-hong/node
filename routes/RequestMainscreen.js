cb6471fb11bf7e7807392ca3fc6bd98cea7900fc
JS-hong <lielty9371@gmail.com>
2021-11-05 19:53:17 +0900

    a

--- routes/RequestMainscreen.js
+++ routes/RequestMainscreen.js
@@ -25,7 +25,6 @@ router.post('/requestmainscreen', function(req,res) {
             var n = 0;
             while(n<5){
                 var random = Math.floor(Math.random()*rows.length);
-                if(randomIndexArray.indexOf(random) === -1){
                     json.push({
                         uid : rows[random].user_id,
                         title : rows[random].subtext,
@@ -39,28 +38,13 @@ router.post('/requestmainscreen', function(req,res) {
                         line_of_code : rows[random].line_of_code,
                         bookmark_saved : rows[random].bookmark_saved,
                         bookmark : rows[random].bookmark
-                        })
-                        
+                    })
+                if(randomIndexArray.indexOf(random) === -1){                        
                         randomIndexArray.push(rows[random].post_id)    
-                        n++;  
+                        n-- 
                 }
                 else{
-                    json.pop({
-                        uid : rows[random].user_id,
-                        title : rows[random].subtext,
-                        post_tag : rows[random].tag,
-                        language_type : rows[random].language_type,
-                        language_thumbnails : rows[random].language_thumbnail,
-                        post_id : rows[random].post_id,
-                        write_time : rows[random].write_time,
-                        writer_nickname : rows[random].writer_nickname,
-                        writer_thumbnail : rows[random].writer_thumbnail,
-                        line_of_code : rows[random].line_of_code,
-                        bookmark_saved : rows[random].bookmark_saved,
-                        bookmark : rows[random].bookmark
-                        })
-                    randomIndexArray.pop(rows[random].post_id)
-                    n--; 
+                    n++
                 }
             }
             res.send({"feeds": json})
