d5df52424875587ef6dc99bb4e5d762ab4f250b1
JS-hong <lielty9371@gmail.com>
2021-11-06 20:26:04 +0900

    a

--- routes/RequestMainscreen.js
+++ routes/RequestMainscreen.js
@@ -54,7 +54,22 @@ router.post('/requestmainscreen', function(req,res) {
                         i++
                     }
                     else{
-                        //중복일때                     
+                        //중복일때
+                        json.pop({
+                            uid : rows[random].user_id,
+                            title : rows[random].subtext,
+                            post_tag : rows[random].tag,
+                            language_type : rows[random].language_type,
+                            language_thumbnails : rows[random].language_thumbnail,
+                            post_id : rows[random].post_id,
+                            write_time : rows[random].write_time,
+                            writer_nickname : rows[random].writer_nickname,
+                            writer_thumbnail : rows[random].writer_thumbnail,
+                            line_of_code : rows[random].line_of_code,
+                            bookmark_saved : rows[random].bookmark_saved,
+                            bookmark : rows[random].bookmark
+                            }) 
+                            randomIndexArray.pop(rows[random].post_id)                     
                         n--
                     }
                 }
@@ -72,6 +87,8 @@ router.post('/requestmainscreen', function(req,res) {
                     bookmark_saved : rows[random].bookmark_saved,
                     bookmark : rows[random].bookmark
                 })
+
+                randomIndexArray.push(rows[random].post_id)
             }
             res.send({"feeds": json})
         }
