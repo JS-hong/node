37f78a4c45776dd5905423afd87d05251faa6cb0
JS-hong <lielty9371@gmail.com>
2021-11-09 20:31:04 +0900

    a

--- routes/PostComment.js
+++ routes/PostComment.js
@@ -2,15 +2,13 @@ const express = require('express');
 const router = express.Router();
 const connection = require('./mysqlconnect');
 
-connection.connect();
-
 router.post('/postcomment', function(req,res) {
 
     const comment = req.body.comment;
     const user_id = req.body.user_id;
     const post_id = req.body.post_id;
 
-    var sql = 'INSERT INTO comment_db(comment,user_id,post_id,comment_like) VALUES(?,?,?,0)';
+    var sql = 'INSERT INTO comment_db(comment,user_id,post_id,comment_like) VALUES(?,?,?,0) ';
 	var params = [comment,user_id,post_id];
 	
 	connection.query(sql,params,function(err, rows){
