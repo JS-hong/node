4fc06909f8cff0a0662d98452751339863810aa7
JS-hong <lielty9371@gmail.com>
2021-11-09 20:18:02 +0900

    a

--- routes/PostComment.js
+++ routes/PostComment.js
@@ -10,16 +10,16 @@ router.post('/postcomment', function(req,res) {
     const user_id = req.body.user_id;
     const post_id = req.body.main;
 
-    var sql = 'INSERT INTO comment_db(comment,user_id,post_id) VALUES(?,?,?)';
+    var sql = 'INSERT INTO comment_db(comment,user_id,post_id,comment_like) VALUES(?,?,?,0)';
 	var params = [comment,user_id,post_id];
 	
 	connection.query(sql,params,function(err, rows){
 		if (err) throw err;
 		if (rows){
-            res.send({data : success});
+            res.send({data : "success"});
 		} 
         else {
-            res.send({data : fail});
+            res.send({data : "fail"});
 		}
 	})
 })
