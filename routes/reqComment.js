15a0acbaa3f468b44722d9e5f1b5c41113992c00
JS-hong <lielty9371@gmail.com>
2021-11-09 21:35:29 +0900

    a

--- routes/reqComment.js
+++ routes/reqComment.js
@@ -5,15 +5,17 @@ const connection = require('./mysqlconnect');
 router.post('/reqcomment', function(req,res) {
 
     var user_id = req.body.user_id;
+	var post_id = req.body.post_id;
     var json = new Object;
     var json = [];
-	var params = [user_id];
+	var params = [user_id,post_id];
 
 	var sql = "select comment_db.comment,comment_db.comment_date, "
 	"IFNULL((comment_like_db.comment_liked AND comment_like_db.user_id=?),0) comment_liked, "
 	"comment_db.post_id,comment_db.comment_like,comment_db.comment_uid, "
 	"from comment_db left join comment_like_db "
 	"on comment_db.comment_uid = comment_like_db.comment_uid "
+	"wher comment_db.post_id = ? "
 	"order by write_time desc ";
 	
 	connection.query(sql,params,function(err, rows){
@@ -26,9 +28,9 @@ router.post('/reqcomment', function(req,res) {
 					uid : rows[i].user_id,
 					post_id : rows[i].language_type,
 					comment_like : rows[i].comment_like,
+					comment_liked : rows[i].comment_liked,
 					comment_uid : rows[i].comment_uid,
-					write_time : rows[i].write_time,
-					writer_nickname : rows[i].writer_nickname,
+					comment_date : rows[i].comment_date,
 					})
 			}
 			res.send({"comment": json})
