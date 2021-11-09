2a81c2e4b743d72cd0c6df1ef63dfe9f04beb905
JS-hong <lielty9371@gmail.com>
2021-11-09 20:47:14 +0900

    a

--- routes/PostComment.js
+++ routes/PostComment.js
@@ -8,8 +8,8 @@ router.post('/postcomment', function(req,res) {
     const user_id = req.body.user_id;
     const post_id = req.body.post_id;
 
-    var sql = 'INSERT INTO comment_db(comment,user_id,post_id,comment_like) VALUES(?,?,?,0) ';
-	var params = [comment,user_id,post_id];
+    var sql = 'INSERT INTO comment_db(comment,user_id,post_id,comment_like) VALUES(?,?,?,?) ';
+	var params = [comment,user_id,post_id,'0'];
 	
 	connection.query(sql,params,function(err, rows){
 		if (err) throw err;
