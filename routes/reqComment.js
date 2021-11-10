b645dd462eef375519ac95307e97556ca0409232
JS-hong <lielty9371@gmail.com>
2021-11-10 19:27:09 +0900

    a

--- routes/reqComment.js
+++ routes/reqComment.js
@@ -4,19 +4,19 @@ const connection = require('./mysqlconnect');
 
 router.post('/reqcomment', function(req,res) {
 
-    //const uid = req.body.user_id;
+    const uid = req.body.user_id;
 	const post_id = req.body.post_id;
 
 	var sql = "select comment_db.comment,comment_db.comment_date, "
-	+ "IFNULL((comment_like_db.comment_liked AND comment_like_db.user_id='TlhscEfljdMEobwWyKfGKKNor143'),0) comment_liked, "
+	+ "IFNULL((comment_like_db.comment_liked AND comment_like_db.user_id=?),0) comment_liked, "
 	+ "comment_db.post_id,comment_db.comment_like,comment_db.comment_uid "
 	+ "from comment_db left join comment_like_db "
 	+ "on comment_db.comment_uid = comment_like_db.comment_uid "
 	+ "where comment_db.post_id=? ";
 	
-	var params = [post_id];
-    var json1 = new Object;
-    var json1 = [];
+	var params = [uid,post_id];
+    var json = new Object;
+    var json = [];
 
 	connection.query(sql,params,function(err, rows){
 		if (err){
@@ -24,7 +24,7 @@ router.post('/reqcomment', function(req,res) {
 		}
 		if (rows){
 			for(i=0;rows.length;i++){
-				json1.push({
+				json.push({
 					comment : rows[i].comment,
 					comment_date : rows[i].comment_date,
 					post_id : rows[i].post_id,
@@ -34,7 +34,7 @@ router.post('/reqcomment', function(req,res) {
 					comment_uid : rows[i].comment_uid,
 					})
 			}
-			res.send({"comment": json1});
+			res.send({"comment": json});
 		} 
         else {
             res.send({data : 'fail'});
